dc0be29527f42315340604120185a9d574b4711c
LaxmaReddy-Nalla <reddylaxman.nalla@gmail.com>
2020-11-09 07:37:14 +0530

    changed path file

--- paths_test.py
+++ paths_test.py
@@ -5,6 +5,8 @@ def getPATH():
     source = Path(inpFilePAth)
     p = PurePosixPath(inpFilePAth)
     q = str(p.parent)
+    r == inpFilePAth = input('Enter file path:- ')
+    source = Path(inpFilePAth)
     print(q)
     fileName = str(source.name)
     print(fileName)
